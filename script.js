aeb0f99feb4d7929cd22f4b03b13b4e5850e451f
mrinish7 <90040174+mrinish7@users.noreply.github.com>
2022-01-31 17:49:54 +0530

    Revert "docupdate"
    This reverts commit cc8681d6cdadeb6ddfd77bd83822c6fbe1f4d1d6.

--- script.js
+++ script.js
@@ -5,26 +5,26 @@ const message = document.querySelector ("#error-message");
 const availableNotes = [2000, 500, 200, 100, 50, 10, 1];
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
-checkButton.addEventListener("click", function calculateBillAndCashAmount() {
-    hideMessage();
-if (billAmount.value > 0) {
-    if (cashGiven.value >= billAmount.value) {
-        const amountToBeReturned = cashGiven.value - billAmount.value;
-        calculateChange(amountToBeReturned);
-    }else {
-        showMessage("Do you wanna wash plates ?")
+checkButton.addEventListener("click", function validatebillandcashamount ()
+{hideMessage();
+    if (billAmount.value > 0) {
+        if (cashGiven.value >= billAmount.value) {
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturned);
+        } else {
+            showMessage("Do you wanna wash plates ?")
+        }
+     } else {
+        showMessage("Invalid bill amount");
     }
-}else {
-    showMessage("Invalid bill amount");
-}
 });
 
 function calculateChange(amountToBeReturned) {
-for (let i = 0; i < availableNotes.length; i++) {
-    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes [i]);
-    amountToBeReturned = amountToBeReturned % availableNotes[i];
-    noOfNotes[i].innertext = numberOfNotes;  
-    }   
+    for ( let i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(amountToBeReturned /  availableNotes [i]);
+        amountToBeReturned = amountToBeReturned % availableNotes[i]; 
+        noOfNotes[i].innerText = numberOfNotes;
+    }
 }
 
 function hideMessage() {
@@ -33,5 +33,5 @@ function hideMessage() {
 
 function showMessage(msg) {
     message.style.display = "block";
-    message.innertext = msg;
-};
+    message.innerText = msg;
+}
